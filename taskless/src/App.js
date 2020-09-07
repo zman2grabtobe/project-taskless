3526f5dd6d6104087e9c32e9131039341d288dcd
zman2grabtobe <zman@grabtobe.com>
2020-09-07 20:11:22 +0900

    ui done

--- taskless/src/App.js
+++ taskless/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper' ;
+import Table from '@material-ui/core/Table' ;
+import TableHead from '@material-ui/core/TableHead' ;
+import TableBody from '@material-ui/core/TableBody' ;
+import TableRow from '@material-ui/core/TableRow' ;
+import TableCell from '@material-ui/core/TableCell' ;
+import { withStyles } from '@material-ui/core/styles' ; 
+
+const styles = withStyles({
+  root: {
+    width : '100%',
+   // marginTop : spacing.unit * 3,
+    overflowX : 'auto'
+
+  },
+  table : {
+    minWidth : 1080
+  }
+})
 
 const customers = [
             {
@@ -41,29 +59,45 @@ const customers = [
             }
 ]
 
-function App() {
 
-  return (
-  <div>
-    {
-       customers.map(c =>{
-                                  return(
-                                    <Customer
-                                      key={c.id}   // key 필수로 넣어야 함 (시퀀스)
-                                      id={c.id}
-                                      image={c.image}
-                                      name={c.name}
-                                      birthday={c.birthday}
-                                      gender={c.gender}
-                                      job={c.job}
-                                    />
-                              
-                                  )
+
+
+function App() {
+ return (
+
+          <Paper >
+            <Table >
+              <TableHead>
+                <TableRow>
+                  <TableCell>번호</TableCell>
+                  <TableCell>사진</TableCell>
+                  <TableCell>이름</TableCell>
+                  <TableCell>생년월일</TableCell>
+                  <TableCell>성별</TableCell>
+                  <TableCell>직업</TableCell>
+                </TableRow>
+              </TableHead>
+            <TableBody>
+                          {
+                            customers.map(c =>{return(<Customer
+                                                        key={c.id}   // key 필수로 넣어야 함 (시퀀스)
+                                                        id={c.id}
+                                                        image={c.image}
+                                                        name={c.name}
+                                                        birthday={c.birthday}
+                                                        gender={c.gender}
+                                                        job={c.job}
+                                                      />
+                                                      )
+                                              }
+                                          )
                            }
-                     )
-    }
-   </div>
+            </TableBody>
+          </Table>
+        </Paper>
   )
-  }
+  
+}
+
 
 export default App;

59bc02c7568d14fe303c899a6f8874480828e666
zman2grabtobe <zman@grabtobe.com>
2020-09-07 18:02:24 +0900

    test

---
+++ taskless/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray_backgroud">
+      <img src={logo} lat="logo"/>
+      <h2>Develop Taskless</h2>
+    </div>
+  );
+}
+
+export default App;
